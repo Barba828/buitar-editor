aa1d2c2060e010669c697148fdbf8913214d49f5
liningzhu <liningzhu@xd.com>
2024-03-01 15:21:24 +0800

    feat: update package info

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: './lib',
+    }),
+  ],
   build: {
     lib: {
       entry: '/lib/index.ts',
