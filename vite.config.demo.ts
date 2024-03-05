8b6ae6267078d7ae2aa7c971168e2d88950d7b2f
liningzhu <liningzhu@xd.com>
2024-03-05 20:36:22 +0800

    feat: build for ghpage

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const baseUrl = process.env.BASE_URL || '/'
+
 export default defineConfig({
   plugins: [react()],
+  base: baseUrl,
   build: {
     outDir: 'dist-demo',
   },
