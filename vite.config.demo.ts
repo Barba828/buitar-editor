9e84fd86b3adc12fc28f12622a5b698c6adca133
liningzhu <liningzhu@xd.com>
2024-04-09 21:27:35 +0800

    ci: scss preprocssorOptions

--- vite.config.demo.ts
+++ vite.config.demo.ts
@@ -6,6 +6,13 @@ const baseUrl = process.env.BASE_URL || '/'
 export default defineConfig({
   plugins: [react()],
   base: baseUrl,
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "/lib/styles/definitions.scss";', // 预处理
+      },
+    },
+  },
   build: {
     outDir: 'dist-demo',
   },
